60d1c1a28d58f35ee8486a8ddbc5a7cedaa08ee8
dw2023 <dwkim126@gmail.com>
2023-03-19 01:27:11 +0900

    Rq 객체 생성을 스프링에 맡김

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -4,33 +4,32 @@ import com.ll.basic1.base.rq.Rq;
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.member.entity.Member;
 import com.ll.basic1.boundedContext.member.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@AllArgsConstructor
 public class MemberController {
     // @Autowired
     // private MemberService memberService;
     // 아래 코드 대신 이것도 가능 (필드주입)
     private final MemberService memberService;
+    private final Rq rq;
 
-        // 생성자 주입
-        @Autowired
-        public MemberController(MemberService memberService) {
-            this.memberService = memberService;
-        }
-
+    /* @AllArgsConstructor로 생략됨
+    @Autowired
+    public MemberController(MemberService memberService, Rq rq) {
+        this.memberService = memberService;
+        this.rq = rq;
+    }
+    */
 
     // 로그인 성공한 회원의 id를 쿠키 값으로 만듦
     @GetMapping("/member/login")
     @ResponseBody
-    public RsData login(String username, String password, HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
-
+    public RsData login(String username, String password) {
         if ( username == null || username.trim().length() == 0 ) {
             return RsData.of("F-3", "username(을)를 입력해주세요.");
         }
@@ -51,8 +50,7 @@ public class MemberController {
 
     @GetMapping("/member/logout") // logout 시 쿠키 지우기 using ChatGPT
     @ResponseBody
-    public RsData logout(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp); // rq에게 맡겨버리기
+    public RsData logout() {
         boolean cookieRemoved = rq.removeCookie("loginedMemberId");
 
         if (cookieRemoved == false) { // 쿠키 삭제 안됨 == name인 쿠키 없음
@@ -65,9 +63,7 @@ public class MemberController {
     //
     @GetMapping("/member/me")
     @ResponseBody
-    public RsData showMe(HttpServletRequest req, HttpServletResponse resp) {
-        Rq rq = new Rq(req, resp);
-
+    public RsData showMe() {
         long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0); // rq에게 쿠키 값 요청
 
         boolean isLogined = loginedMemberId > 0; // 0보다 크면 로그인된 것임
