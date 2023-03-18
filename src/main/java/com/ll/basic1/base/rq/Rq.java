dea72242216cd8f5579907296593eb3bde923d08
dw2023 <dwkim126@gmail.com>
2023-03-18 22:51:02 +0900

    removeCookie 개선

--- src/main/java/com/ll/basic1/base/rq/Rq.java
+++ src/main/java/com/ll/basic1/base/rq/Rq.java
@@ -12,26 +12,30 @@ public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
 
+    // 더 좋은 코드로 개선
     // 삭제가 성공했는지 t/f로 리턴
     public boolean removeCookie(String name) {
         if (req.getCookies() != null) {
-            Arrays.stream(req.getCookies())
-                    .filter(cookie -> cookie.getName().equals(name))
-                    .forEach(cookie -> {
-                        cookie.setMaxAge(0);
-                        resp.addCookie(cookie);
-                    });
+            // 쿠키 찾기
+            Cookie cookie = Arrays.stream(req.getCookies())
+                    .filter(c -> c.getName().equals(name))
+                    .findFirst()
+                    .orElse(null);
 
-            return Arrays.stream(req.getCookies())
-                    .filter(cookie -> cookie.getName().equals(name)) // name인 쿠키가 있으면 위 if문에서 삭제됐을 것이기 때문에
-                    .count() > 0; // name인 쿠키가 0개 이상 있으면 삭제된 것 -> true 리턴
+            // 쿠키를 찾았다면 삭제 후 t 리턴
+            if (cookie != null) {
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+
+                return true;
+            }
         }
 
         return false;
     }
 
     public String getCookie(String name, String defaultValue) {
-        if ( req.getCookies() == null ) return defaultValue;
+        if (req.getCookies() == null) return defaultValue;
 
         return Arrays.stream(req.getCookies())
                 .filter(cookie -> cookie.getName().equals(name)) // 전체 쿠키에서 name인 쿠키 찾기
@@ -44,14 +48,13 @@ public class Rq {
     public long getCookieAsLong(String name, long defaultValue) {
         String value = getCookie(name, null); // 이름이 name인 쿠키의 값 가져옴
 
-        if ( value == null ) {
+        if (value == null) {
             return defaultValue;
         }
 
         try {
             return Long.parseLong(value); // ctrl 누르고 parseLong 클릭하면 발생할 예외 알 수 있음
-        }
-        catch ( NumberFormatException e ) {
+        } catch (NumberFormatException e) {
             return defaultValue;
         }
     }
