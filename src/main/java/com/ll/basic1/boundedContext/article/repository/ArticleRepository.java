3ba6ba1547e23d0f384c314f290c44efc28f9b7d
dw2023 <dwkim126@gmail.com>
2023-03-20 08:28:43 +0900

    ArticleService 도입

--- src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.ll.basic1.boundedContext.article.repository;
 import com.ll.basic1.boundedContext.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// ArticleRepository는 JPA가 구현함
 // 이 클래스에는 @Repository 어노테이션을 생략해도 된다.
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     // ArticleRepository는 Article 테이블을 다룬다
