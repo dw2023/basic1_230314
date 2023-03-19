041e829640bff6dba46ddb2790c879246da694a2
dw2023 <dwkim126@gmail.com>
2023-03-20 08:07:51 +0900

    JPA 를 통해서 insert

---
+++ src/main/java/com/ll/basic1/boundedContext/article/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package com.ll.basic1.boundedContext.article.repository;
+
+import com.ll.basic1.boundedContext.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 이 클래스에는 @Repository 어노테이션을 생략해도 된다.
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    // ArticleRepository는 Article 테이블을 다룬다
+    // Article의 Primary key는 Long타입이다.
+}
