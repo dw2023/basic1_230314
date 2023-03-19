9d523fe6873581546b995cca717e1f147b63cc4c
dw2023 <dwkim126@gmail.com>
2023-03-20 05:01:25 +0900

    로그인 폼의 발송방식을 post로 변경

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.ll.basic1.boundedContext.member.service.MemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -32,7 +33,7 @@ public class MemberController {
         return "usr/member/login";
     }
 
-    @GetMapping("/member/doLogin")
+    @PostMapping("/member/login") // Post로 method를 바꿨기 때문에 위에 것과 url이 같아도 됨
     @ResponseBody
     public RsData login(String username, String password) {
         if ( username == null || username.trim().length() == 0 ) {
