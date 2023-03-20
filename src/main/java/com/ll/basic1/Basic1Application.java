fbbdf365b70d93eb6965ccc033e4191428678696
dw2023 <dwkim126@gmail.com>
2023-03-21 07:58:23 +0900

    등록날짜, 수정날짜 자동갱신

--- src/main/java/com/ll/basic1/Basic1Application.java
+++ src/main/java/com/ll/basic1/Basic1Application.java
@@ -2,8 +2,10 @@ package com.ll.basic1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // @EntityListeners(AuditingEntityListener.class) 작동시켜줌
 public class Basic1Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,16 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -17,11 +21,14 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate, @LastModifiedDate 를 사용하려면 써야 함
 public class Article {
     @Id // id를 primary key로 설정
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private long id;
+    @CreatedDate // 데이터 생성될 때 자동으로 날짜 넣어줌
     private LocalDateTime createDate; // 데이터 생성 날짜
+    @LastModifiedDate // 데이터 수정될 때 자동으로 날짜 넣어줌
     private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
